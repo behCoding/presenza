a68c5bcc261bcf7dcae5c47888a149a6e09e04fc
behrad <behcoding@gmai.com>
2024-12-19 11:46:08 +0100

    Css changes

--- frontend/presence-tracker/src/components/Login.js
+++ frontend/presence-tracker/src/components/Login.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import './Login.css';
- 
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "./Login.css";
+
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const emailSuffix = '@storelink.it';
+  const emailSuffix = "@storelink.it";
   const [showSuggestions, setShowSuggestions] = useState(false);
 
   const handleEmailChange = (e) => {
@@ -16,7 +16,11 @@ const Login = () => {
     setUsername(value);
 
     // Show suggestions if no "@" is present
-    setShowSuggestions(!value.includes('@'));
+    if (value.length > 3) {
+      setShowSuggestions(!value.includes("@"));
+    } else {
+      setShowSuggestions(false);
+    }
   };
 
   const handleSuggestionClick = () => {
@@ -24,78 +28,76 @@ const Login = () => {
     setShowSuggestions(false);
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setError('');
-    
-    try {
-      const response = await axios.post('http://localhost:8000/token', new URLSearchParams({
-        username: username,
-        password: password,
-      }), {
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-      });
 
-      const { access_token, role, user_id} = response.data;
-      localStorage.setItem('token', access_token);
-      localStorage.setItem('user_id', user_id);
-      // Redirect based on role
-      if (role === 'admin') {
-        navigate('/admin-dashboard');
-      }
-      else {
-        navigate('/employee-dashboard');
-      }
-    } catch (error) {
-      console.error('Error logging in:', error);
-      setError('Login failed. Please check your credentials and try again.');
-    }
+    toast
+      .promise(
+        axios.post(
+          "http://localhost:8000/token",
+          new URLSearchParams({
+            username: username,
+            password: password,
+          }),
+          {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          }
+        ),
+        {
+          pending: "Logging in...",
+          success: "Login successful!",
+          error: "Login failed. Please check your credentials and try again.",
+        }
+      )
+      .then((response) => {
+        const { access_token, role, user_id } = response.data;
+        localStorage.setItem("token", access_token);
+        localStorage.setItem("user_id", user_id);
+
+        if (role === "admin") {
+          navigate("/admin-dashboard");
+        } else {
+          navigate("/employee-dashboard");
+        }
+      })
+      .catch((error) => {
+        console.error("Error logging in:", error);
+      });
   };
 
   return (
-    <div>
+    <div className="login_container">
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
-      <div style={{ position: 'relative' }}>
+        <div className="login_inputContainer">
+          <div className="login_email">
+            <input
+              type="text"
+              placeholder="Enter your email"
+              value={username}
+              onChange={handleEmailChange}
+            />
+            {showSuggestions && (
+              <div className="login_suggestion" onClick={handleSuggestionClick}>
+                {username}@storelink.it
+              </div>
+            )}
+          </div>
+
           <input
-            type="text"
-            placeholder="Enter your email"
-            value={username}
-            onChange={handleEmailChange}
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            onFocus={() => setShowSuggestions(false)}
           />
-          {showSuggestions && (
-            <div
-              style={{
-                position: 'absolute',
-                backgroundColor: 'white',
-                border: '1px solid #ccc',
-                borderRadius: '4px',
-                width: '100%',
-                zIndex: 10,
-                cursor: 'pointer',
-              }}
-              onClick={handleSuggestionClick}
-            >
-              {username}@storelink.it
-            </div>
-          )}
+          <p>
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
         </div>
-        
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
         <button type="submit">Login</button>
       </form>
-      {error && <p>{error}</p>}
-
-      <button onClick={() => navigate('/register')} style={{ marginTop: '10px' }}>
-        Register
-      </button>
     </div>
-
   );
 };
 
